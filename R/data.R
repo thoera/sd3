fb6bf8e6b97bdfb730b867bf1ec1b925ddc907f9
thoera <erard.thomas@gmail.com>
2019-03-01 21:53:29 +0100

    First release

---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' Exemple de parcours agrégés.
+#'
+#' Un dataset contenant des parcours agrégés dans une unique colonne.
+#'
+#' @format Un dataframe avec 10 lignes et 2 variables :
+#' \describe{
+#'   \item{parcours}{les parcours}
+#'   \item{comptage}{le comptage associé}
+#' }
+"data_1"
+
+#' Exemple de parcours agrégés.
+#'
+#' Un dataset contenant des parcours agrégés dans des colonnes distinctes.
+#'
+#' @format Un dataframe avec 10 lignes et 6 variables :
+#' \describe{
+#'   \item{contact_1}{le premier élément du parcours}
+#'   \item{contact_2}{le second élément du parcours}
+#'   \item{contact_3}{le troisième élément du parcours}
+#'   \item{contact_4}{le quatrième élément du parcours}
+#'   \item{contact_5}{le cinquième et dernier élément du parcours}
+#'   \item{comptage}{le comptage associé}
+#' }
+"data_2"
+
+#' Exemple de parcours individuels.
+#'
+#' Un dataset contenant des parcours individuels dans une unique colonne.
+#'
+#' @format Un dataframe avec 42 lignes et 2 variables :
+#' \describe{
+#'   \item{id}{l'identifiant de l'individu}
+#'   \item{parcours}{les parcours}
+#' }
+"data_3"
+
+#' Exemple de parcours individuels.
+#'
+#' Un dataset contenant des parcours individuels dans des colonnes distinctes.
+#'
+#' @format Un dataframe avec 42 lignes et 2 variables :
+#' \describe{
+#'   \item{id}{l'identifiant de l'individu}
+#'   \item{contact_1}{le premier élément du parcours}
+#'   \item{contact_2}{le second élément du parcours}
+#'   \item{contact_3}{le troisième élément du parcours}
+#'   \item{contact_4}{le quatrième élément du parcours}
+#'   \item{contact_5}{le cinquième et dernier élément du parcours}
+#' }
+"data_4"
